9dae8313f94b9714ba0e2b8f5d5a8757d3a507d4
codehero <awalansarmentor@gmail.com>
2022-11-13 19:50:30 +0000

    added get started

--- src/.vuepress/.temp/internal/pagesRoutes.js
+++ src/.vuepress/.temp/internal/pagesRoutes.js
@@ -1,12 +1,12 @@
 export const pagesRoutes = [
   ["v-8daa1a0e","/",{"t":"Short Code Africa Docs","i":"home"},["/index.html","/README.md"]],
   ["v-2e3eac9e","/slides.html",{"t":"Slide page","i":"slides"},["/slides","/slides.md"]],
-  ["v-fffb8e28","/guide/",{"t":"Guide","i":"creative"},["/guide/index.html","/guide/README.md"]],
   ["v-1473bf53","/demo/",{"t":"Features Demo","i":"discover"},["/demo/index.html","/demo/README.md"]],
   ["v-4e65ec78","/demo/disable.html",{"t":"Disabling layout and features","i":"config"},["/demo/disable","/demo/disable.md"]],
   ["v-c151bf32","/demo/encrypt.html",{"t":"Encryption Article","i":"lock"},["/demo/encrypt","/demo/encrypt.md"]],
   ["v-438ffe52","/demo/markdown.html",{"t":"Markdown Enhance","i":"markdown"},["/demo/markdown","/demo/markdown.md"]],
   ["v-6e19edb7","/demo/page.html",{"t":"Page Config","i":"page"},["/demo/page","/demo/page.md"]],
+  ["v-fffb8e28","/guide/",{"t":"Guide","i":"creative"},["/guide/index.html","/guide/README.md"]],
   ["v-9b97feb4","/guide/GettingStarted/",{"t":"Getting Started"},["/guide/GettingStarted/index.html","/guide/GettingStarted/README.md"]],
   ["v-3706649a","/404.html",{"t":""},["/404"]],
 ]
--- src/.vuepress/.temp/pages/index.html.vue
+++ src/.vuepress/.temp/pages/index.html.vue
@@ -1,6 +1,6 @@
-<template><div><p>This is an example of a project homepage. You can place your main content here.</p>
-<p>To use this layout, you need to set <code v-pre>home: true</code> in the page front matter.</p>
-<p>For related descriptions of configuration items, please see <a href="https://vuepress-theme-hope.github.io/v2/guide/layout/home/" target="_blank" rel="noopener noreferrer">Project HomePage Layout Config<ExternalLinkIcon/></a>.</p>
+<template><div><!-- This is an example of a project homepage. You can place your main content here. -->
+<!-- To use this layout, you need to set `home: true` in the page front matter. -->
+<!-- For related descriptions of configuration items, please see [Project HomePage Layout Config](https://vuepress-theme-hope.github.io/v2/guide/layout/home/). -->
 </div></template>
 
 
