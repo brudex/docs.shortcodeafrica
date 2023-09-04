a424ed55af5a221f47de3d34827d60bb9d3440e7
brudex <brudexgh@gmail.com>
2023-09-04 23:30:08 +0000

    added link to app

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -2,7 +2,7 @@ import { navbar } from "vuepress-theme-hope";
 
 export const enNavbar = navbar([
   // "/",
-  { text: "Open App", icon: "home", link: "https://shortcodeafrica.com" }
+  { text: "Open App", icon: "home", link: "https://shortcodeafrica.com/app-list" }
 ]);
  
 
